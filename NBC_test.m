93fdd7030aa4cda7f4cfb739a1b43750e21f157e
bssthu <bssthu@users.noreply.github.com>
2015-06-25 00:35:22 +0800

    use time to decide

--- NBC_test.m
+++ NBC_test.m
@@ -10,7 +10,6 @@ function correct_rate = NBC_test(testData, testLabels, theta)
 
 %% Init
 num_class = 20;
-num_action = 9;
 num_match = 380;    % [(比赛ID - 190) * 2 - 主客队] 作为这里的 match_id
 num_match_half = 190;
 N_data = size(testData, 1);
@@ -23,12 +22,15 @@ test_data = testData(:, [1, 4]);  % 第一列为比赛ID，第二列为动作
 test_data(:, 1) = (testData(:, 1) - num_match_half) * 2 - testData(:, 2);
 
 %% Estimate
-theta_log = log(theta);
+theta_log = log(theta + 0.000001);
+time_frame = ceil(testData(:, 5) / 2700);
+time_frame(time_frame == 0) = 1;
 for i = 1:N_data
     match_id = test_data(i, 1);
     action = test_data(i, 2);
+    act_type = action * time_frame(i);
     for j = 1:num_class
-        p_match(match_id, j) = p_match(match_id, j) + theta_log(j, action);
+        p_match(match_id, j) = p_match(match_id, j) + theta_log(j, act_type);
     end
 end
 
--- NBC_train.m
+++ NBC_train.m
@@ -11,11 +11,12 @@ function theta = NBC_train(trainData, trainLabels)
 %% Init
 num_class = 20;
 num_action = 9;
+num_timeframe = 2;
 N_data = size(trainData, 1);
 N_label = size(trainLabels, 1);
 
-theta = zeros(num_class, num_action);
-action_count = zeros(num_class, num_action);
+theta = zeros(num_class, num_action * num_timeframe);
+action_count = zeros(num_class, num_action * num_timeframe);
 
 %% Preprocess
 train_data = trainData(:, [1, 4]);  % 第一列为球队ID，第二列为动作
@@ -27,10 +28,15 @@ for i = 1:N_label
 end
 
 %% Train
+time_frame = ceil(trainData(:, 5) / 2700);
+time_frame(time_frame == 0) = 1;
 for i = 1:num_class
     for j = 1:num_action
-        I = (train_data(:, 1) == i) .* (train_data(:, 2) == j);
-        action_count(i, j) = sum(I);
+        for k = 1:num_timeframe
+            I = (train_data(:, 1) == i) .* (train_data(:, 2) == j);
+            I = I .* (time_frame == k);
+            action_count(i, j * k) = sum(I);
+        end
     end
     theta(i, :) = action_count(i, :) / sum(action_count(i, :));
 end
